6bed7ba39fc762e95ac78043f88f9bcd3bcfb2c7
Joa Riski <riski.joa@gmail.com>
2022-06-10 22:59:02 +0300

    Remove auth

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from notes.api.serializers import LocationModelSerializer, TaskModelSerializer
@@ -8,13 +8,13 @@ from notes.models import Location, Task
 
 class LocationViewSet(ModelViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = Location.objects.all()
     serializer_class = LocationModelSerializer
 
 
 class TaskViewSet(ModelViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = Task.objects.all()
     serializer_class = TaskModelSerializer
