3ec44dfffd723a7c71663e9fab0d464b75f7cefe
William Lindholm <william.lindholm@ander.com>
2022-06-10 18:27:46 +0300

    Update models with range

--- notes/api/serializers.py
+++ notes/api/serializers.py
@@ -1,33 +1,42 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 
 from notes.models import Category, Group, Location, LocationGroup, Task
 
 
-class LocationModelSerializer(HyperlinkedModelSerializer):
+class LocationModelSerializer(ModelSerializer):
     class Meta:
         model = Location
         fields = ("longitude", "latitude", "name")
 
 
-class LocationGroupModelSerializer(HyperlinkedModelSerializer):
+class LocationGroupModelSerializer(ModelSerializer):
     class Meta:
         model = LocationGroup
         fields = ("locations", "name")
 
 
-class CategoryModelSerializer(HyperlinkedModelSerializer):
+class CategoryModelSerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ("name",)
+        fields = ("name", "range")
 
 
-class TaskModelSerializer(HyperlinkedModelSerializer):
+class TaskModelSerializer(ModelSerializer):
     class Meta:
         model = Task
-        fields = ("title", "description", "due_date", "location_group", "creator", "assigned_group")
+        fields = (
+            "title",
+            "description",
+            "due_date",
+            "location_group",
+            "creator",
+            "assigned_group",
+            "range",
+            "category",
+        )
 
 
-class GroupModelSerializer(HyperlinkedModelSerializer):
+class GroupModelSerializer(ModelSerializer):
     class Meta:
         model = Group
         fields = ("name", "creator", "members")
--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from notes.api.views import CategoryViewSet, LocationGroupViewSet, LocationViewSet, TaskViewSet
+from notes.api.views import CategoryViewSet, GroupViewSet, LocationGroupViewSet, LocationViewSet, TaskViewSet
 
 router = routers.DefaultRouter()
 router.register(r"locations", LocationViewSet)
 router.register(r"tasks", TaskViewSet)
 router.register(r"categories", CategoryViewSet)
 router.register(r"location-groups", LocationGroupViewSet)
+router.register(r"groups", GroupViewSet)
