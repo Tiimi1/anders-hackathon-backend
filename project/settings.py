2462ef67cfb24500482e95c87cd8be2420384b1e
Joa Riski <riski.joa@gmail.com>
2022-06-10 15:36:17 +0300

    Fix staticfiles storage

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
